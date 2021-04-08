57a1f5a19c5fc1c4cc8f2d47dab2f621f1cbeb7c
khs6226 <khsun6226@naver.com>
2021-04-07 22:41:08 -0700

    change the db connection info

--- databaseConnection.js
+++ databaseConnection.js
@@ -4,9 +4,9 @@ const is_heroku = process.env.IS_HEROKU || false;
 
 const dbConfigHeroku = {
 	host: "us-cdbr-east-03.cleardb.com",
-	user: "bad5cf78b26163",
-	password: "361d72ee",
-	database: "heroku_b0b033c41088a0b",
+	user: "b9de27540d7da6",
+	password: "16403fef",
+	database: "heroku_8da364bc4e311c8",
 	multipleStatements: false,
   namedPlaceholders: true
 };
@@ -15,7 +15,7 @@ const dbConfigLocal = {
 	host: "localhost",
 	user: "root",
 	password: "7116226",
-	database: "lab_example",
+	database: "restaurant_review",
 	multipleStatements: false,
   namedPlaceholders: true
 };
